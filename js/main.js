53878b488e608a375a3a0d8e19bba66f531e49ed
ariefulkhakim <ariefulkhakim24@gmail.com>
2017-02-16 17:28:03 +0700

    creating portfolio grid

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
-$(function () { 
-  $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
-});  
 
-$( window ).scroll(function() {   
- if($( window ).scrollTop() > 10){  // scroll down abit and get the action   
-  $(".progress-bar").each(function(){
-    each_bar_width = $(this).attr('aria-valuenow');
-    $(this).width(each_bar_width + '%');
-  });
-       
-  }  
-});
+
+	$(function () { 
+	  $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
+	});  
+
+	$( window ).scroll(function() {   
+		if($( window ).scrollTop() > 10){  // scroll down abit and get the action   
+			$(".progress-bar").each(function(){
+			each_bar_width = $(this).attr('aria-valuenow');
+	    	$(this).width(each_bar_width + '%');
+	  	});   
+	  	}  
+	});
+
+	$('.portfolio-wrapper').mixItUp();
+
+	$('.portfolio-popup').magnificPopup({
+	  	type: 'image',
+	  		gallery: {
+	      		enabled: true
+	  		},
+	  	zoom: {
+	      	enabled: true,
+	      	duration: 300, // don't foget to change the duration also in CSS
+	      	opener: function(element) {
+	          return element.find('img');
+	      	}
+	  	}
+	});
+
+
