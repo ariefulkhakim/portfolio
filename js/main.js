b27188b810bbbe79111a0b165f54a55ede830c5f
ariefulkhakim <ariefulkhakim24@gmail.com>
2017-02-27 16:37:12 +0700

    finish build website and revisi

--- js/main.js
+++ js/main.js
@@ -29,6 +29,21 @@
 	  	}
 	});
 
+	// Scroll-Top
+	$(window).on('scroll', function() {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+    $('.scrollup').on("click", function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
 
 	});
 
